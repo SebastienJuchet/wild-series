0e129e69cc84b51b7c359d80dfcca60bc28d8b72
Sebastien Juchet <sebastien.juchet@gmail.com>
2021-11-29 21:29:25 +0100

    Create entities Category and Program

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,14 +6,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+/**
+ * @Route("/program", name="program_")
+ */
 class ProgramController extends AbstractController
 {
     /**
-    * @Route("/program")
+    * @Route("/", name="index")
     */
     public function index(): Response
     {
         return $this->render('program/index.html.twig');
+    }
+
+    /**
+     * @Route("/{id}/", name="show", methods="GET", requirements={"id"="\d{1,}"})
+     */
+    public function show(int $id)
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
+        ]);
     }
 }
